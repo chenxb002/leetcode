a61b1114695842db2afc5e2b5059d7f3d992c120
chenxiaobing <chenxb002@whu.edu.cn>
2018-08-02 03:35:05 +0800

    feat(82): 82 accepted

--- 82_remove_duplicates_from_sorted_list_ii.cpp
+++ 82_remove_duplicates_from_sorted_list_ii.cpp
@@ -10,17 +10,22 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode *dummy = new ListNode(0);
+        dummy->next = NULL;
         ListNode *tail = dummy;
         ListNode *cur = head;
         while (cur) {
             if (cur->next && cur->val == cur->next->val) {
-               int val = cur->val;
-               while (cur && cur->val == val) { cur = cur->next; }
+                int val = cur->val;
+                while (cur && cur->val == val) { cur = cur->next; }
             } else {
-               tail->next = cur;
-               tail = tail->next;
+                tail->next = cur;
+                tail = tail->next;
+                cur = cur->next;
             }
         }
+        tail->next = NULL;
         return dummy->next;
     }
 };
+
+
